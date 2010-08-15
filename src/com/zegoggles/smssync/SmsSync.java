dcfbc383fdc446b2cbbf7e6b0bb0d0e668258288
Jan Berkel <jan.berkel@gmail.com>
2010-08-15 16:14:44 +0200

    Update status onResume()
      Closes GH-27.

--- src/com/zegoggles/smssync/SmsSync.java
+++ src/com/zegoggles/smssync/SmsSync.java
@@ -142,6 +142,8 @@ public class SmsSync extends PreferenceActivity {
         updateImapFolderLabelFromPref();
         updateMaxItemsPerSync(null);
         updateMaxItemsPerRestore(null);
+
+        mStatusPref.update();
     }
 
     @Override
