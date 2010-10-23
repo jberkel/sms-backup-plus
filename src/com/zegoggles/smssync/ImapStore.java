3d06037ab118150b8a8af70ee941009ba7aea820
Jan Berkel <jan.berkel@gmail.com>
2010-10-23 17:34:48 +0200

    fix npe

--- src/com/zegoggles/smssync/ImapStore.java
+++ src/com/zegoggles/smssync/ImapStore.java
@@ -83,7 +83,8 @@ public class ImapStore extends com.fsck.k9.mail.store.ImapStore {
                 //Debug.startMethodTracing("sorting");
                 Arrays.sort(msgs, new Comparator<Message>() {
                     public int compare(Message m1, Message m2) {
-                        return (m2 != null && m2.getSentDate() != null) ?
+                        return (m2 != null && m2.getSentDate() != null &&
+                                m1 != null && m1.getSentDate() != null) ?
                                 m2.getSentDate().compareTo(m1.getSentDate()) : -1;
                     }
                 });
