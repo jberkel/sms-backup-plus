133662ebf60488c6b36e3e51050924a6679397d9
Jan Berkel <jan.berkel@gmail.com>
2010-11-08 02:34:36 +0100

    Updated license headers

--- src/com/zegoggles/smssync/CursorToMessage.java
+++ src/com/zegoggles/smssync/CursorToMessage.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2009 Christoph Studer <chstuder@gmail.com>
+ * Copyright (c) 2010 Jan Berkel <jan.berkel@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/com/zegoggles/smssync/SmsBroadcastReceiver.java
+++ src/com/zegoggles/smssync/SmsBroadcastReceiver.java
@@ -1,4 +1,5 @@
 /* Copyright (c) 2009 Christoph Studer <chstuder@gmail.com>
+ * Copyright (c) 2010 Jan Berkel <jan.berkel@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.zegoggles.smssync;
 
 import android.content.BroadcastReceiver;
