589aa104c3f77c4b186b61cbe73be0f1ab6495fe
Jan Berkel <jan.berkel@gmail.com>
2010-12-27 06:55:34 +0100

    Made all Consts classes interfaces

--- src/com/zegoggles/smssync/SmsConsts.java
+++ src/com/zegoggles/smssync/SmsConsts.java
@@ -20,43 +20,41 @@ package com.zegoggles.smssync;
  * Contains SMS content provider constants. These values are copied from
  * com.android.provider.telephony.*
  */
-public class SmsConsts {
+public interface SmsConsts {
 
-    public static final String ID = "_id";
+     String ID = "_id";
 
-    public static final String BODY = "body";
+     String BODY = "body";
 
-    public static final String DATE = "date";
+     String DATE = "date";
 
-    public static final String THREAD_ID = "thread_id";
+     String THREAD_ID = "thread_id";
 
-    public static final String ADDRESS = "address";
+     String ADDRESS = "address";
 
-    public static final String TYPE = "type";
+     String TYPE = "type";
 
-    public static final String READ = "read";
+     String READ = "read";
 
-    public static final String STATUS = "status";
+     String STATUS = "status";
 
-    public static final String SERVICE_CENTER = "service_center";
+     String SERVICE_CENTER = "service_center";
 
-    public static final String PROTOCOL = "protocol";
+     String PROTOCOL = "protocol";
 
-    public static final String PERSON = "person";
+     String PERSON = "person";
 
-    public static final int MESSAGE_TYPE_ALL = 0;
+     int MESSAGE_TYPE_ALL = 0;
 
-    public static final int MESSAGE_TYPE_INBOX = 1;
+     int MESSAGE_TYPE_INBOX = 1;
 
-    public static final int MESSAGE_TYPE_SENT = 2;
+     int MESSAGE_TYPE_SENT = 2;
 
-    public static final int MESSAGE_TYPE_DRAFT = 3;
+     int MESSAGE_TYPE_DRAFT = 3;
 
-    public static final int MESSAGE_TYPE_OUTBOX = 4;
+     int MESSAGE_TYPE_OUTBOX = 4;
 
-    public static final int MESSAGE_TYPE_FAILED = 5; // for failed outgoing
-                                                     // messages
+     int MESSAGE_TYPE_FAILED = 5; // for failed outgoing messages
 
-    public static final int MESSAGE_TYPE_QUEUED = 6; // for messages to send
-                                                     // later
+     int MESSAGE_TYPE_QUEUED = 6; // for messages to send later
 }
