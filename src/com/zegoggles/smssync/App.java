468d3c9b0b8c04ad64fced73b2b804e5079cd7bd
Jan Berkel <jan.berkel@gmail.com>
2011-10-18 21:37:02 +0200

    Remove unused checkReportSender

--- src/com/zegoggles/smssync/App.java
+++ src/com/zegoggles/smssync/App.java
@@ -25,7 +25,7 @@ import android.util.Config;
 import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
 
-@ReportsCrashes(formUri = "https://bugsense.appspot.com/api/acra?api_key=a2603e16", formKey = "",  checkReportSender = true)
+@ReportsCrashes(formUri = "https://bugsense.appspot.com/api/acra?api_key=a2603e16", formKey = "")
 public class App extends Application {
     public static final boolean DEBUG = false;
     public static final boolean LOCAL_LOGV = App.DEBUG ? Config.LOGD : Config.LOGV;
