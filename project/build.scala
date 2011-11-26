ca8fcd863b14dcfecf8b2c5ae3612d503f034bc3
Jan Berkel <jan.berkel@gmail.com>
2011-11-26 01:14:34 +0100

    Updated build scripts, added sbt support

---
+++ project/build.scala
@@ -0,0 +1,51 @@
+import sbt._
+import Keys._
+import AndroidKeys._
+
+object General {
+  val settings = Defaults.defaultSettings ++ Seq(
+    version := "1.4.4-SNAPSHOT",
+    organization := "com.zegoggles"
+  )
+
+  val androidSettings =
+    settings ++
+    Seq(
+      platformName := "android-10"
+    )
+
+  val androidProjectSettings =
+    androidSettings ++
+    AndroidProject.androidSettings ++
+    PlainJavaProject.settings
+}
+
+object AndroidBuild extends Build {
+  val coreDependencies = Seq(
+  )
+
+  val providedDependencies = Seq(
+    "com.google.android" % "android" % "2.3.3" % "provided",
+    "org.apache.httpcomponents" % "httpcore" % "4.0.1" % "provided",
+    "org.apache.httpcomponents" % "httpclient" % "4.0.3" % "provided",
+    "org.json" % "json" % "20090211" % "provided",
+    "commons-logging" % "commons-logging" % "1.1.1" % "provided",
+    "commons-codec" % "commons-codec" % "1.5" % "provided"
+  )
+
+  val testDependencies = Seq()
+
+  val repos = Seq(
+    MavenRepository("acra release repository", "http://acra.googlecode.com/svn/repository/releases")
+  )
+
+  lazy val soundcloud_android = Project (
+    "sms-backup-plus",
+    file("."),
+    settings = General.androidProjectSettings ++ Seq (
+      keyalias in Android := "jberkel",
+      libraryDependencies ++= coreDependencies ++ providedDependencies ++ testDependencies,
+      resolvers ++= repos
+    ) ++ AndroidInstall.settings
+  )
+}
---
+++ project/plugins/build.sbt
@@ -0,0 +1,1 @@
+addSbtPlugin("org.scala-tools.sbt" % "sbt-android-plugin" % "0.6.0")
