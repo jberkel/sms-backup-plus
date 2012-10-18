eda08225273351997adfa4d87e178e5e70f0580c
Hampus Bengtsson <hampus.bengtsson@gmail.com>
2012-10-18 12:58:29 +0200

    Restoring most recent items first

--- src/com/zegoggles/smssync/BackupImapStore.java
+++ src/com/zegoggles/smssync/BackupImapStore.java
@@ -135,6 +135,8 @@ public class BackupImapStore extends ImapStore {
                 Collections.addAll(messages, msgs);
             }
 
+            Collections.reverse(messages);
+            
             return messages;
         }
 
