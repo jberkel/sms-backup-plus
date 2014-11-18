7c5a74aa8d372f468b74c6f403dd9a52c2844a90
Jan Berkel <jan.berkel@gmail.com>
2014-11-18 18:34:24 +0000

    Add empty implementations to avoid crashes while loading
        java.lang.RuntimeException: Unable to instantiate activity ComponentInfo{com.zegoggles.smssync/com.zegoggles.smssync.ComposeSmsActivity}: java.lang.ClassNotFoundException: Didn't find class "com.zegoggles.smssync.ComposeSmsActivity" on path: DexPathList[[zip file "/data/app/com.zegoggles.smssync-2.apk"],nativeLibraryDirectories=[/data/app-lib/com.zegoggles.smssync-2, /vendor/lib, /system/lib]]

---
+++ src/main/java/com/zegoggles/smssync/compat/ComposeSmsActivity.java
@@ -0,0 +1,6 @@
+package com.zegoggles.smssync.compat;
+
+import android.app.Activity;
+
+public class ComposeSmsActivity extends Activity {
+}
---
+++ src/main/java/com/zegoggles/smssync/compat/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The purpose of this package is compatibility with Kitkat+ versions
+ * of Android. All the implementations are stubs and not actually used.
+ */
+package com.zegoggles.smssync.compat;
