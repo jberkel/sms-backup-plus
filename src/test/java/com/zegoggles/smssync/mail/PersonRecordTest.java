7418dd441e876dc3b784a7c874af4cdc944b36da
Jan Berkel <jan.berkel@gmail.com>
2015-09-20 11:13:17 +0100

    Formatting

--- src/test/java/com/zegoggles/smssync/mail/PersonRecordTest.java
+++ src/test/java/com/zegoggles/smssync/mail/PersonRecordTest.java
@@ -10,41 +10,35 @@ import static org.fest.assertions.api.Assertions.assertThat;
 @RunWith(RobolectricTestRunner.class)
 public class PersonRecordTest {
 
-
-    @Test
-    public void shouldSanitizeInputData() throws Exception {
+    @Test public void shouldSanitizeInputData() throws Exception {
         PersonRecord r = new PersonRecord(1, "foo\n\r\n", "foo\n@gmail.com", "\r\r1234");
 
         assertThat(r.getEmail()).isEqualTo("foo@gmail.com");
         assertThat(r.getName()).isEqualTo("foo");
         assertThat(r.getNumber()).isEqualTo("1234");
     }
-    @Test
-    public void shouldBeUnknownForInvalidIds() throws Exception {
+
+    @Test public void shouldBeUnknownForInvalidIds() throws Exception {
         PersonRecord record = new PersonRecord(0, null, null, "-1");
         assertThat(record.isUnknown()).isTrue();
     }
 
-    @Test
-    public void shouldNotBeUnknownForValidIds() throws Exception {
+    @Test public void shouldNotBeUnknownForValidIds() throws Exception {
         PersonRecord record = new PersonRecord(12, null, null, "-1");
         assertThat(record.isUnknown()).isFalse();
     }
 
-    @Test
-    public void shouldReturnNumberForUnknown() throws Exception {
+    @Test public void shouldReturnNumberForUnknown() throws Exception {
         PersonRecord record = new PersonRecord(1, null, null, "-1");
         assertThat(record.getNumber()).isEqualTo("Unknown");
     }
 
-    @Test
-    public void shouldReturnUnknownEmail() throws Exception {
+    @Test public void shouldReturnUnknownEmail() throws Exception {
         PersonRecord record = new PersonRecord(0, null, null, "-1");
         assertThat(record.getEmail()).isEqualTo("unknown.number@unknown.email");
     }
 
-    @Test
-    public void shouldGetAddress() throws Exception {
+    @Test public void shouldGetAddress() throws Exception {
         PersonRecord record = new PersonRecord(1, "John Appleseed", "john@appleseed.com", "+141543432");
         assertThat(record.getAddress(AddressStyle.NAME_AND_NUMBER).toString()).isEqualTo(
              "\"John Appleseed (+141543432)\" <john@appleseed.com>");
@@ -56,8 +50,7 @@ public class PersonRecordTest {
                 "+141543432 <john@appleseed.com>");
     }
 
-    @Test
-    public void shouldGetAddressMissingEmail() throws Exception {
+    @Test public void shouldGetAddressMissingEmail() throws Exception {
         PersonRecord record = new PersonRecord(1, "John Appleseed", null, "+141543432");
 
         assertThat(record.getAddress(AddressStyle.NAME_AND_NUMBER).toString()).isEqualTo(
@@ -70,8 +63,7 @@ public class PersonRecordTest {
                 "+141543432 <+141543432@unknown.email>");
     }
 
-    @Test
-    public void shouldGetAddressMissingName() throws Exception {
+    @Test public void shouldGetAddressMissingName() throws Exception {
         PersonRecord record = new PersonRecord(1, null, "john@appleseed.com", "+141543432");
 
         assertThat(record.getAddress(AddressStyle.NAME_AND_NUMBER).toString()).isEqualTo(
@@ -84,8 +76,7 @@ public class PersonRecordTest {
                 "+141543432 <john@appleseed.com>");
     }
 
-    @Test
-    public void shouldGetAddressMissingNumber() throws Exception {
+    @Test public void shouldGetAddressMissingNumber() throws Exception {
         PersonRecord record = new PersonRecord(1, "John Appleseed", "john@appleseed.com", null);
         assertThat(record.getAddress(AddressStyle.NAME_AND_NUMBER).toString()).isEqualTo(
                 "\"John Appleseed (Unknown)\" <john@appleseed.com>");
