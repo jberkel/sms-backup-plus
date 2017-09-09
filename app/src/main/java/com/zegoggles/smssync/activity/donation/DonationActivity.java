b333ba4c5ce2a92d55c664cfd109f0f3ee04dc5d
Jan Berkel <jan@berkel.fr>
2017-09-09 20:24:29 +0200

    Fix IAB

--- app/src/main/java/com/zegoggles/smssync/activity/donation/DonationActivity.java
+++ app/src/main/java/com/zegoggles/smssync/activity/donation/DonationActivity.java
@@ -12,7 +12,6 @@ import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.BillingClientStateListener;
 import com.android.billingclient.api.BillingFlowParams;
 import com.android.billingclient.api.Purchase;
-import com.android.billingclient.api.PurchaseHistoryResponseListener;
 import com.android.billingclient.api.PurchasesUpdatedListener;
 import com.android.billingclient.api.SkuDetails;
 import com.android.billingclient.api.SkuDetailsResponseListener;
@@ -49,27 +48,24 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
         super.onCreate(savedInstanceState);
         billingClient = new BillingClient.Builder(this).setListener(this).build();
         billingClient.startConnection(new BillingClientStateListener() {
-            String message;
             @Override
             public void onBillingSetupFinished(int resultCode) {
+                Log.d(TAG, "onBillingSetupFinished(" + resultCode + ")" + Thread.currentThread().getName());
                 switch (resultCode) {
-                    case BILLING_UNAVAILABLE:
-                        message = getString(R.string.donation_error_iab_unavailable);
-                        break;
                     case OK:
-                        List<String> moreSkus = new ArrayList<String>();
-                        Collections.addAll(moreSkus, ALL_SKUS);
-                        billingClient.querySkuDetailsAsync(INAPP, moreSkus, DonationActivity.this);
-
+                        queryAvailableSkus();
+                        break;
                     default:
+                        String message = getString(BILLING_UNAVAILABLE);
+                        Toast.makeText(DonationActivity.this, message, Toast.LENGTH_LONG).show();
+                        Log.w(TAG, "Problem setting up in-app billing: " + resultCode);
+                        finish();
+                        break;
                 }
-                Toast.makeText(DonationActivity.this, message, Toast.LENGTH_LONG).show();
-                Log.w(TAG, "Problem setting up in-app billing: " + resultCode);
-                finish();
             }
-
             @Override
             public void onBillingServiceDisconnected() {
+                Log.d(TAG, "onBillingServiceDisconnected");
             }
         });
     }
@@ -85,6 +81,7 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
     @Override
     public void onSkuDetailsResponse(SkuDetails.SkuDetailsResult result) {
         log("onQueryInventoryFinished(" + result + ", " + result + ")");
+
         if (result.getResponseCode() != OK) {
             Log.w(TAG, "failed to query inventory: " + result);
             return;
@@ -96,27 +93,19 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
                 skuDetailsList.add(d);
             }
         }
-        /*
-        if (DEBUG_IAB) {
-            Purchase testPurchase = result.getPurchase(TestSkus.PURCHASED.getSku());
-            if (testPurchase != null) {
-                billingClient.consumeAsync(testPurchase, new ConsumeResponseListener() {
-                    @Override
-                    public void onConsumeResponse(String purchaseToken, int resultCode) {
-                        Log.d(TAG, "onConsumeFinished:" + purchaseToken + ", " + resultCode);
-                    }
-                });
-            }
-        }
-        */
 
-        if (!isFinishing() && !userHasDonated(result.getSkuDetailsList())) {
+        if (!isFinishing()) {
             showSelectDialog(skuDetailsList);
         } else {
             finish();
         }
     }
 
+    private void queryAvailableSkus() {
+        List<String> moreSkus = new ArrayList<String>();
+        Collections.addAll(moreSkus, ALL_SKUS);
+        billingClient.querySkuDetailsAsync(INAPP, moreSkus, DonationActivity.this);
+    }
 
     private void showSelectDialog(List<SkuDetails> skuDetails) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -124,21 +113,37 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
         final List<SkuDetails> skus = new ArrayList<SkuDetails>(skuDetails);
         Collections.sort(skus, SkuComparator.INSTANCE);
         //noinspection ConstantConditions
-        String[] items = new String[skus.size()];
-        for (int i = 0; i < skus.size(); i++) {
-            final SkuDetails sku = skus.get(i);
+        List<String> options = new ArrayList<String>();
+        if (DEBUG_IAB) {
+            for (TestSkus.TestSku testSku : TestSkus.SKUS) {
+                options.add(testSku.getDescription());
+            }
+        }
+        for (final SkuDetails sku : skus) {
             String item = sku.getTitle();
             if (!TextUtils.isEmpty(sku.getPrice())) {
                 item += "  " + sku.getPrice();
             }
-            items[i] = item;
+            options.add(item);
         }
+        String[] items = new String[options.size()];
+        options.toArray(items);
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                String selectedSku;
+                if (DEBUG_IAB) {
+                    if (which < TestSkus.SKUS.length) {
+                        selectedSku = TestSkus.SKUS[which].getSku();
+                    } else {
+                        selectedSku = skus.get(which - TestSkus.SKUS.length).getSku();
+                    }
+                } else {
+                    selectedSku = skus.get(which).getSku();
+                }
                 billingClient.launchBillingFlow(DonationActivity.this,  new BillingFlowParams.Builder()
                         .setType(INAPP)
-                        .setSku(skus.get(which).getSku())
+                        .setSku(selectedSku)
                         .build()
                 );
             }
@@ -155,8 +160,7 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
                 finish();
             }
         });
-        builder.setTitle(R.string.ui_dialog_donate_message)
-                .show();
+        builder.setTitle(R.string.ui_dialog_donate_message).show();
     }
 
     @Override
@@ -179,9 +183,8 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
                 case USER_CANCELED:
                     message = getString(R.string.donation_error_canceled);
                     break;
-
                 default:
-                    message = "Unspecified error";
+                    message = getString(R.string.donation_unspecified_error,    responseCode);
             }
             Toast.makeText(this,
                     getString(R.string.ui_donation_failure_message, message),
@@ -189,28 +192,6 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
                     .show();
         }
         finish();
-    }
-
-    private static boolean userHasDonatedPurchases(List<Purchase> result) {
-        for (String sku : ALL_SKUS) {
-            for (Purchase purchase : result) {
-                if (purchase.getSku().equals(sku)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private static boolean userHasDonated(List<SkuDetails> result) {
-        for (String sku : ALL_SKUS) {
-            for (SkuDetails details : result) {
-                if (details.getSku().equals(sku)) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     private void log(String s) {
@@ -230,31 +211,43 @@ public class DonationActivity extends Activity implements SkuDetailsResponseList
     }
 
     public static void checkUserHasDonated(Context c, final DonationStatusListener l) {
-        final BillingClient helper = new BillingClient.Builder(c).build();
+        final BillingClient helper = new BillingClient.Builder(c).setListener(new PurchasesUpdatedListener() {
+            @Override
+            public void onPurchasesUpdated(int responseCode, List<Purchase> purchases) {
+                Log.d(TAG, "onPurchasesUpdated("+responseCode+")");
+            }
+        }).build();
         helper.startConnection(new BillingClientStateListener() {
             @Override
             public void onBillingSetupFinished(int resultCode) {
+                Log.d(TAG, "checkUserHasDonated: onBillingSetupFinished("+resultCode+")");
                 if (resultCode == OK) {
-                    helper.queryPurchaseHistoryAsync(INAPP, new PurchaseHistoryResponseListener() {
-                        @Override
-                        public void onPurchaseHistoryResponse(Purchase.PurchasesResult result) {
-                            if (result.getResponseCode() == OK) {
-                                final State s = userHasDonatedPurchases(result.getPurchasesList()) ? DONATED : NOT_DONATED;
-                                l.userDonationState(s);
-                            } else {
-                                l.userDonationState(UNKNOWN);
-                            }
-                            helper.endConnection();
-                        }
-                    });
+                    Purchase.PurchasesResult result = helper.queryPurchases(INAPP);
+                    if (result.getResponseCode() == OK) {
+                        final State s = userHasDonated(result.getPurchasesList()) ? DONATED : NOT_DONATED;
+                        l.userDonationState(s);
+                    } else {
+                        l.userDonationState(UNKNOWN);
+                    }
                 } else {
                     l.userDonationState(resultCode == BILLING_UNAVAILABLE ? NOT_AVAILABLE : UNKNOWN);
-                    helper.endConnection();
                 }
+                helper.endConnection();
             }
             public void onBillingServiceDisconnected() {
             }
         });
+    }
+
+    private static boolean userHasDonated(List<Purchase> result) {
+        for (String sku : ALL_SKUS) {
+            for (Purchase purchase : result) {
+                if (purchase.getSku().equals(sku)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private static class SkuComparator implements Comparator<SkuDetails> {
