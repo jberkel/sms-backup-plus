d9ef5baaa430c08b5ed75abb85b3b628abf0ca92
Jan Berkel <jan@berkel.fr>
2017-12-20 19:24:54 +0100

    Set incoming default to 60secs

--- app/src/main/java/com/zegoggles/smssync/preferences/Defaults.java
+++ app/src/main/java/com/zegoggles/smssync/preferences/Defaults.java
@@ -12,7 +12,7 @@ class Defaults {
     /**
      * Default value for {@link Preferences#INCOMING_TIMEOUT_SECONDS}.
      */
-    public static final int INCOMING_TIMEOUT_SECONDS = 60 * 3;
+    public static final int INCOMING_TIMEOUT_SECONDS = 60;
     /**
      * Default value for {@link Preferences#REGULAR_TIMEOUT_SECONDS}.
      */
