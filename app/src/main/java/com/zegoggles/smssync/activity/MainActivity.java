e4009c83471bc3f54275ed7c0a55d0ed1c42b901
Jan Berkel <jan@berkel.fr>
2018-01-03 22:39:06 +0100

    Inline

--- app/src/main/java/com/zegoggles/smssync/activity/MainActivity.java
+++ app/src/main/java/com/zegoggles/smssync/activity/MainActivity.java
@@ -199,8 +199,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
